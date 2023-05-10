aa004ca8c2fa5a6e184d438e64e6a959986d33df
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-05-10 01:46:34 -0400

    create separate navbar component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
-import logo from './logo.png';
+import Navbar from './Navbar';
 
 function Header() {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [isSmallDevice, setIsSmallDevice] = useState(false);
   const [toggleLogo, setToggleLogo] = useState(true);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
+    handleDeviceSize();
+    window.addEventListener("resize", handleDeviceSize);
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleDeviceSize);
     };
   }, []);
 
@@ -19,119 +22,35 @@ function Header() {
     setScrollPosition(newScrollPosition);
   };
 
-  if (scrollPosition > 110 && toggleLogo) {
-    setToggleLogo(false);
-  } else if (scrollPosition < 10 && !toggleLogo) {
-    setToggleLogo(true);
-  }
+  const handleDeviceSize = () => {
+    const width = window.innerWidth;
+    setIsSmallDevice(width <= 768);
+  };
 
-  let logoWidth = toggleLogo ? 6 : 4;
+  if (scrollPosition > 110 && toggleLogo)
+    setToggleLogo(false);
+  else if (scrollPosition < 10 && !toggleLogo)
+    setToggleLogo(true);
+
+    
   let opacity = toggleLogo ? 1 : 0;
+
+  if (isSmallDevice) {
+
+  }
+  else {
+
+  }
 
   return (
     <>
     <div id="header">
-      <nav className="navbar">
-        <ul className="navbar__list">
-          <li className="navbar__item">
-            <Link to="/" className="navbar__link navbar__brand">
-              ABOUT
-            </Link>
-          </li>
-          <li className="navbar__item navbar__dropdown pillars-dropdown">
-            <NavLink activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
-              PILLARS
-            </NavLink>
-            <ul className="navbar__sublist">
-              <li className="navbar__subitem">
-                <NavLink to="/dance" activeclassname="navbar__link--active" className="navbar__link">
-                  DANCE
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/culture" activeclassname="navbar__link--active" className="navbar__link">
-                  CULTURE
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/sports" activeclassname="navbar__link--active" className="navbar__link">
-                  SPORTS
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/philanthropy" activeclassname="navbar__link--active" className="navbar__link">
-                  PHILANTHROPY
-                </NavLink>
-              </li>
-            </ul>
-          </li>
-          <li className="navbar__item navbar__dropdown pillars-dropdown">
-            <NavLink activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
-              PROGRAMS
-            </NavLink>
-            <ul className="navbar__sublist">
-              <li className="navbar__subitem">
-                <NavLink to="/aka" activeclassname="navbar__link--active" className="navbar__link">
-                  AKA / RAKA
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/maffia" activeclassname="navbar__link--active" className="navbar__link">
-                  MAFFIA
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/fsa+" activeclassname="navbar__link--active" className="navbar__link">
-                  FSA+
-                </NavLink>
-              </li>
-            </ul>
-          </li>
-        </ul>
-
-        <div className="navbar-logo">
-          <Link to="/">
-            <img src={logo} id="logo" alt="Logo" style={{ width: `${logoWidth}rem` }} />
-          </Link>
-        </div>
-
-        <ul className="navbar__list">
-          <li className="navbar__item navbar__dropdown">
-            <NavLink to="/contact" activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
-              EVENTS
-            </NavLink>
-          </li>
-          <li className="navbar__item navbar__dropdown pillars-dropdown">
-            <NavLink activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
-              MEDIA
-            </NavLink>
-            <ul className="navbar__sublist">
-              <li className="navbar__subitem">
-                <NavLink to="/barkada" activeclassname="navbar__link--active" className="navbar__link">
-                  BARKADA NEWSLETTER
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/project-lunchbox" activeclassname="navbar__link--active" className="navbar__link">
-                  PROJECT LUNCHBOX
-                </NavLink>
-              </li>
-              <li className="navbar__subitem">
-                <NavLink to="/archive" activeclassname="navbar__link--active" className="navbar__link">
-                  ARCHIVE
-                </NavLink>
-              </li>
-            </ul>
-          </li>
-          <li className="navbar__item navbar__dropdown">
-            <NavLink to="/merch" activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
-              MERCHANDISE
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <Navbar 
+        scrollPosition = {scrollPosition}
+        isSmallDevice = {isSmallDevice}
+        toggleLogo = {toggleLogo}
+      />
       
-
       <div id="background" className="fade-out" style={{ opacity: opacity }}>
         <div id="divider" />
       </div>
---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import logo from './logo.png';
+
+function Navbar(props) {
+    const { scrollPosition, isSmallDevice, toggleLogo } = props;
+
+    let list1 = (
+        <ul className="navbar__list">
+          <li className="navbar__item">
+            <Link to="/" className="navbar__link navbar__brand">
+              ABOUT
+            </Link>
+          </li>
+          <li className="navbar__item navbar__dropdown pillars-dropdown">
+            <NavLink activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
+              PILLARS
+            </NavLink>
+            <ul className="navbar__sublist">
+              <li className="navbar__subitem">
+                <NavLink to="/dance" activeclassname="navbar__link--active" className="navbar__link">
+                  DANCE
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/culture" activeclassname="navbar__link--active" className="navbar__link">
+                  CULTURE
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/sports" activeclassname="navbar__link--active" className="navbar__link">
+                  SPORTS
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/philanthropy" activeclassname="navbar__link--active" className="navbar__link">
+                  PHILANTHROPY
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="navbar__item navbar__dropdown pillars-dropdown">
+            <NavLink activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
+              PROGRAMS
+            </NavLink>
+            <ul className="navbar__sublist">
+              <li className="navbar__subitem">
+                <NavLink to="/aka" activeclassname="navbar__link--active" className="navbar__link">
+                  AKA / RAKA
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/maffia" activeclassname="navbar__link--active" className="navbar__link">
+                  MAFFIA
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/fsa+" activeclassname="navbar__link--active" className="navbar__link">
+                  FSA+
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      );
+    
+      let list2 = (
+        <ul className="navbar__list">
+          <li className="navbar__item navbar__dropdown">
+            <NavLink to="/contact" activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
+              EVENTS
+            </NavLink>
+          </li>
+          <li className="navbar__item navbar__dropdown pillars-dropdown">
+            <NavLink activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
+              MEDIA
+            </NavLink>
+            <ul className="navbar__sublist">
+              <li className="navbar__subitem">
+                <NavLink to="/barkada" activeclassname="navbar__link--active" className="navbar__link">
+                  BARKADA NEWSLETTER
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/project-lunchbox" activeclassname="navbar__link--active" className="navbar__link">
+                  PROJECT LUNCHBOX
+                </NavLink>
+              </li>
+              <li className="navbar__subitem">
+                <NavLink to="/archive" activeclassname="navbar__link--active" className="navbar__link">
+                  ARCHIVE
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="navbar__item navbar__dropdown">
+            <NavLink to="/merch" activeclassname="navbar__link--active" className="navbar__link navbar__dropdown-toggle">
+              MERCHANDISE
+            </NavLink>
+          </li>
+        </ul>
+      );
+
+      let logoWidth = toggleLogo ? 6 : 4;
+
+      let navbar;
+
+      if (isSmallDevice) {
+        navbar = (
+            <nav className="navbar">
+                <div className="navbar-logo">
+                    <Link to="/">
+                        <img src={logo} id="logo" alt="Logo" style={{ width: '3rem' }} />
+                    </Link>
+                </div>
+                <ul className="navbar__list">
+                    {list1.props.children}
+                    {list2.props.children}
+                </ul>
+            </nav>
+        );
+      }
+      else {
+        navbar = (
+            <nav className="navbar">
+                {list1}
+                <div className="navbar-logo">
+                    <Link to="/">
+                        <img src={logo} id="logo" alt="Logo" style={{ width: `${logoWidth}rem` }} />
+                    </Link>
+                </div>
+                {list2}
+            </nav>
+        );
+      }
+
+    return (
+        <>
+            {navbar}
+        </>
+    );
+}
+
+export default Navbar;
