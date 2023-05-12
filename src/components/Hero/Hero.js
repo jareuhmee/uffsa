3330d37a238931cd50ecda133bff04048be357d8
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-05-12 05:47:49 -0400

    added typewriter effect

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Hero.css'
 
-const Hero = ({ color, background, title, subtitle, text }) => {
+const Hero = ({ color, background, title, subtitle, text, children }) => {
     return (
       <section className="hero" style={{ backgroundColor: color }}>
         <div className="hero-background" style={{ backgroundImage: `url(${background})` }}></div>
         <h1 className="hero-title">{title}</h1>
         <h1 className="hero-subtitle">{subtitle}</h1>
-        <br />
+        <br /><br />
         <p className="hero-text">{text}</p>
       </section>
     );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,61 +1,68 @@
 import React from 'react';
-import './Home.css'
-import './Flag.css';
-
+import Typewriter from "typewriter-effect";
 import Hero from '../../components/Hero/Hero';
 import Cards from '../../components/Cards/Cards';
-
+import './Home.css'
+import './Flag.css';
 import backgroundImage from './background.png';
 
 function Home() {
     return (
       <>
-      <Hero 
-        color = "var(--red)"
-        background = {backgroundImage}
-        title = "Filipino Student Association"
-        subtitle = "at the University of Florida"
-        text = "A home away from home for anyone, not just Filipinos."
-      >
-        <p>Isang Pamilya. Isang Diwa. Isang Mahal.</p>
-      </Hero>
-      
-      <div id="about">
-        <h1 className="title">About Us</h1>
-        <br></br>
-        <p>
-          The Filipino Student Association (FSA) at the University of Florida is a home away from home for anyone, not just Filipinos. <br/><br/>
+        <Hero 
+          color = "var(--red)"
+          background = {backgroundImage}
+          title = "Filipino Student Association"
+          subtitle = "at the University of Florida"
+          text = {
+            <Typewriter
+              options={{
+                strings: ['Isang Pamilya.', 'Isang Diwa.', 'Isang Mahal.'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          }
+        >
+          
 
-          From the Def Talent Jam dance competition in the fall to the Barrio Fiesta cultural show in the spring, FSA hosts a wide variety of 
-          events that immerse its members in Filipino culture, both modern and traditional. In addition to hosting dance and cultural events, 
-          FSA also participates in intramural sports such as basketball, flag football, volleyball, tennis, and many more. FSA is also devoted 
-          to giving back to the community and to those in need through their philanthropy fundraisers, events, and month-long event, Gawad 
-          Kalinga month. <br/><br/>
+        </Hero>
+        
+        <div id="about">
+          <h1 className="title">About Us</h1>
+          <br></br>
+          <p>
+            The Filipino Student Association (FSA) at the University of Florida is a home away from home for anyone, not just Filipinos. 
 
-          FSA is dance.<br/><br/>
-          FSA is culture.<br/><br/>
-          FSA is sports.<br/><br/>
-          FSA is philanthropy.<br/><br/>
+            From the Def Talent Jam dance competition in the fall to the Barrio Fiesta cultural show in the spring, FSA hosts a wide variety of 
+            events that immerse its members in Filipino culture, both modern and traditional. In addition to hosting dance and cultural events, 
+            FSA also participates in intramural sports such as basketball, flag football, volleyball, tennis, and many more. FSA is also devoted 
+            to giving back to the community and to those in need through their philanthropy fundraisers, events, and month-long event, Gawad 
+            Kalinga month. 
 
-          And above all else, FSA is family.
-        </p>
-      </div>
+            FSA is dance. 
+            FSA is culture. 
+            FSA is sports. 
+            FSA is philanthropy. 
 
-      <div id="home-pillars">
-        <h1 className='title'> Our Pillars</h1>
-        <Cards />
-
-      </div>
-
-      
-      <div id="section-flag">
-        <div className="flag-container">
-          <div className="triangle"></div>
-          <div className="trapezoid top"></div>
-          <div className="trapezoid bottom"></div>
+            And above all else, FSA is family.
+          </p>
         </div>
-      </div>
 
+        <div id="home-pillars">
+          <h1 className='title'> Our Pillars</h1>
+          <Cards />
+
+        </div>
+
+        
+        <div id="section-flag">
+          <div className="flag-container">
+            <div className="triangle"></div>
+            <div className="trapezoid top"></div>
+            <div className="trapezoid bottom"></div>
+          </div>
+        </div>
       </>
     );
 }
