708a9099d6c5470cdfe2144aee67cbef4e1df717
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-05-21 20:50:38 -0400

    adjusted headings

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
         <section className="events-section" id="events">
           <div className="events-container">
             
-            <h1 className="title">Events</h1>
+            <h1 className="events-title title">Events</h1>
             <iframe
               src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23F5C52A&ctz=America%2FNew_York&title=UFFSA%20Calendars&showTitle=0&showTz=0&showPrint=0&showTabs=0&src=YWFzdS51ZkBnbWFpbC5jb20&src=dWZmc2EuZGFuY2VAZ21haWwuY29t&color=%23D50000&color=%234285F4"
               className="calendar"
