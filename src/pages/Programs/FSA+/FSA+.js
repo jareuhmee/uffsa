7af3d7990eb7acaf8a7795a597007d2c8f8457be
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-05-31 03:53:38 -0400

    begin fsa+ design

--- src/pages/Programs/FSA+/FSA+.js
+++ src/pages/Programs/FSA+/FSA+.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebook, FaLinkedin } from 'react-icons/fa';
 import FsaPlusTitle from '../../../components/titles/FSA+ Title/FsaPlusTitle';
 
 import './FSA+.css';
@@ -9,20 +10,67 @@ const FsaPlus = () => {
 
           <section className='fsaplus-hero'>
             <FsaPlusTitle />
+
+            <div className='fsaplus-hero__buttons'>
+              <div className='fsaplus-hero__fb-buttons'>
+                <a href='https://www.facebook.com/groups/fsaacademics/' target="_blank" rel="noopener noreferrer">
+                  <button>
+                    <FaFacebook /> FSA+
+                  </button>
+                </a>
+
+                <a href='https://www.facebook.com/groups/UFFSA.Alumni/' target="_blank" rel="noopener noreferrer">
+                  <button>
+                    <FaFacebook /> UFFSA Alumni
+                  </button>
+                </a>
+
+                <a href='https://www.facebook.com/groups/FSAnetworking/' target="_blank" rel="noopener noreferrer">
+                  <button>
+                    <FaFacebook /> FSA Networking
+                  </button>
+                </a>
+              </div>
+
+              <a href='https://www.linkedin.com/company/filipino-student-association-at-uf/' target="_blank" rel="noopener noreferrer">
+                <button>
+                  <FaLinkedin /> Filipino Student Association at UF
+                </button>
+              </a>
+            </div>
           </section>
 
 
-          <div className='fsaplus-description'>
-            <div className='temp-text-container'>
-              FSA+ Description: Our academic and career support program<br /><br />
-              Embed Class Connector Form & Spreadsheet (Members can submit schedules to find others taking same courses) <br /><br />
-              Study Social Dates — Collaborate with Membership <br /><br />
-              FB Pages: FSA+ FaceBook, FSA Networking, UFFSA Alumni <br /><br />
-              <a href='https://www.linkedin.com/company/filipino-student-association-at-uf/'>UFFSA LinkedIn</a> <br /><br />
-              Asian-Interest Professional Organizations: UF HEAL, UF SASE, UF AAUSLA, UF ABSA <br /><br />
-              Other Resources: Scholarships, internships, drive of packets/notes <br /><br />
+          <section className='fsaplus-description'>
+
+            <div className='fsaplus-sticky-container'>
+                <div className='fsaplus-sticky-note'>
+                  <h1 className='title'> Study Socials: </h1>
+                  <p className='title'> Next Event: TBD </p>
+                </div>
+
+                <div className='fsaplus-sticky-note'>
+                  <h1 className='title'> Class Connector </h1>
+                  <p className='title'> Form </p>
+                  <p className='title'> Spreadsheet </p>
+                </div>
+
+                <div className='fsaplus-sticky-note'>
+                  <h1 className='title'> FSA+ Library </h1>
+                  <p className='title'> Submission Form </p>
+                  <p className='title'> Google Drive of Notes </p>
+                </div>
             </div>
-          </div>
+
+            <div className='temp-text-container'>
+              FSA+ Description: Our career & academic support program<br /><br />
+              Group chats (use uffsa discord): class recommendations, sharing surveys for papers/research projects, requested majors/tracks  <br /><br />
+              Asian-Interest Professional Organizations: UF HEAL, UF SASE, UF AAUSLA, UF ABSA <br /><br />
+              Other Resources: Scholarships, internships/research, career fairs, study edge, smokin notes  <br /><br />
+            </div>
+
+          </section>
+
         </div>
     </> 
   );
