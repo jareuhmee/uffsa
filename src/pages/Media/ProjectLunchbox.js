5876f63c57285377f99c9f62e83e2248eef62c4f
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-06-20 00:57:56 -0500

    added project lunchbox

--- src/pages/Media/ProjectLunchbox.js
+++ src/pages/Media/ProjectLunchbox.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import logo from './logo.jpg';
+import './ProjectLunchbox.css';
 
 const ProjectLunchbox = () => {  
     return (<>
-        <div>
-          <div className="maffia-description">
-            <div className='temp-text-container'>
-              Embed podcast <br /><br />
-              Link spotify and soundcloud <br /><br />
-              Link philanthropy subpage, MAFFIA, chairs <br /><br />
-            </div>
-          </div>
+
+      <section className='projectLunchbox-hero'>
+        {/* <img src={logo} alt='Logo' /> */}
+        <p>PROJECT</p>
+        <p>LUNCHBOX</p>
+      </section>
+    
+      <section className='projectLunchbox-section'>
+        <div className='podcast-container'>
+          <iframe
+            src="https://open.spotify.com/embed/show/4S2aANBcTLopN467oTCX8W?utm_source=generator"
+            allowFullScreen=""
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            className='podcast'
+          />
         </div>
-    </> 
-  );
+
+
+        {/* <div className="maffia-description">
+          <div className='temp-text-container'>
+            Embed podcast <br /><br />
+            Link spotify and soundcloud <br /><br />
+            Link philanthropy subpage, MAFFIA, chairs <br /><br />
+          </div>
+        </div> */}
+      </section>
+
+
+    </>
+    );
 }
 
 export default ProjectLunchbox;
