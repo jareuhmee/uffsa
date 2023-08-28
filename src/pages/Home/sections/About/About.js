33fc9b1cc08c91904489be3fd4176214138aafa7
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-08-28 15:38:24 -0400

    updated about

--- src/pages/Home/sections/About/About.js
+++ src/pages/Home/sections/About/About.js
@@ -24,18 +24,20 @@ const About = () => {
 
         <div className='about-text'>
           <h2 className='about-text__intro'>
-            The Filipino Student Association (FSA) at the University of Florida is a home away from home for anyone, not just Filipinos. 
+            The Filipino Student Association (FSA) at the University of Florida is a home away from home for all students, whether you're Filipino or not. 
           </h2>
           
           <p>
-            From the Def Talent Jam dance competition in the fall to the Barrio Fiesta cultural show in the spring, FSA hosts a wide variety of 
-            events that immerse its members in Filipino culture, both modern and traditional. In addition to hosting dance and cultural events, 
-            FSA also participates in intramural sports such as basketball, flag football, volleyball, tennis, and many more. FSA is also devoted 
-            to giving back to the community and to those in need through their philanthropy fundraisers, events, and month-long event, Gawad 
-            Kalinga month. 
+            You're not only building friendships that last a lifetime in FSA, you're joining a 
+            pamilya. From the Def Talent Jam dance competition to the Barrio 
+            Fiesta cultural show, from the annual Adobowl tournament to Gawad Kalinga Month, FSA hosts a wide variety of events that immerse its 
+            members into our four pillars: dance, culture, sports, and philanthropy. 
           </p>
-          <p>FSA is dance. FSA is culture. FSA is sports. FSA is philanthropy.</p>
-          <p>And above all else, FSA is family.</p>
+          <p>Since there are so many ways to get involved, it is easy for members to find their niche within FSA.</p>
+          
+          <p className='about-text__outro'>
+            Join the pamilya today!
+          </p>
         </div>
         
       </div>
