483c4d3eb6df3b531c02a71d99a07fe5105b4651
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-09-06 18:28:09 -0400

    update aka

--- src/pages/Programs/AKA/AKA.js
+++ src/pages/Programs/AKA/AKA.js
@@ -9,14 +9,37 @@ const AKA = () => {
           <AKATitle color="white" />
 
           <div className='aka-description'>
-            <div className='temp-text-container'>
-              AKA Description: Our big/little mentorship program <br /><br />
-              Embed application <br /><br />
-              Event info on AKA/RAKA weeks and woodser <br /><br />
-              Feature Pair of the Month + link application <br /><br />
-              Embed Pamilya Spotlights from Instagram <br /><br />
+            <div className='aka-description__container'>
+                AKA (Ate/Kuya/Ading) is the Filipino Student Association's mentorship program here at UF. Our Membership Team will do their best to pair you with an "Ading" (little) or a "Kuya" (big brother) or an "Ate" (big sister) that best suits you!
+                <br /><br />
+                We encourage you to be as specific and descriptive as possible so we can create the best pamilya for you!
+                <br /><br />
+
+                Deadline to submit: FRIDAY, SEPTEMBER 15th @ 11:59PM
+                <br /><br />
+                Once you submit your application, find out your pairing(s) at AKA Reveal after FAHM Opening Ceremony on September 24th at Lake Wauburg. We hope to see you there!
+                <br /><br />
+
+                <a href='https://docs.google.com/forms/d/e/1FAIpQLSeiQclNIP2ruwBGAadFk7GMakF7EDzTUiocgvXreGh34KAzzw/viewform' target="_blank" rel="noopener noreferrer">
+                  APPLY NOW!
+                </a>
+              </div>
+
+              {/* <div className='temp-text-container'>
+                AKA Description: Our big/little mentorship program <br /><br />
+                Embed application <br /><br />
+                Event info on AKA/RAKA weeks and woodser <br /><br />
+                Feature Pair of the Month + link application <br /><br />
+                Embed Pamilya Spotlights from Instagram <br /><br />
+                AKA (Ate/Kuya/Ading) is the Filipino Student Association's mentorship program here at UF. Our Membership Team will do their best to pair you with an "Ading" (little) or a "Kuya" (big brother) or an "Ate" (big sister) that best suits you!
+
+                We encourage you to be as specific and descriptive as possible so we can create the best pamilya for you!
+
+                Deadline to submit: FRIDAY, SEPTEMBER 15th @ 11:59PM
+
+                Once you submit your application, find out your pairing(s) at AKA Reveal after FAHM Opening Ceremony on September 24th at Lake Wauburg. We hope to see you there!
+              </div> */}
             </div>
-          </div>
 
         </div>
     </> 
