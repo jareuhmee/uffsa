7f7d2ade1b2e20b009f1ae4c9cb596a4c893f619
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2023-09-06 12:46:29 -0400

    updated maffia

--- src/pages/Programs/MAFFIA/MAFFIA.js
+++ src/pages/Programs/MAFFIA/MAFFIA.js
@@ -7,13 +7,35 @@ const MAFFIA = () => {
     return (<>
         <div className='maffia-page'>
           <MaffiaTitle color='white' />
-          <div className="maffia-description">
-            <div className='temp-text-container'>
+
+          <section className="maffia-description">
+            <div className='maffia-description__container'>
+              <p>
+                MAFFIA (Membership And FSA First-year Involvement Alliance) is the perfect way for new members to get involved and find their niche within FSA. Within it they will gain firsthand leadership experience and develop valuable skills all while connecting with new and old members of the PAMILYA!
+              </p>
+
+              <p>
+                This program gives the opportunity to network and learn how FSA runs behind the scenes with the help of E-Board members and Chairs. Although MAFFIA is geared towards new members and Freshmen, ALL are welcome and encouraged to join! 
+              </p>
+
+              <p>
+                MAFFIA is divided into 4 sub-programs: Culture, Mass Communications, Multimedia, and Philanthropy. Over the course of the Fall semester, members will be distributed amongst these sub-programs and will rotate every GBM/show so each MAFFIA member has the opportunity to experience each program. 
+              </p>
+
+              <br />
+
+              <p className='maffia-application'>
+                APPLICATION COMING SOON!
+              </p>
+            </div>
+            
+            {/* <div className='temp-text-container'>
               MAFFIA Description: Our first-year involvement program (Membership and FSA First-year Involvement Alliance) <br /><br />
               Embed Application <br /><br />
               Describe Rotations, feature recent work: Multimedia, Culture, Mass Communications, Philanthropy <br /><br />
-            </div>
-          </div>
+            </div> */}
+          </section>
+
         </div>
     </> 
   );
