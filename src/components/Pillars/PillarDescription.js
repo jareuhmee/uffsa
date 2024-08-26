b86b363bf5edfcfc7569f85495c78b1ab6eeef49
mjtady <mjtady@email.com>
2024-08-26 15:55:40 -0400

    changes to culture pg. and text alignment

--- src/components/Pillars/PillarDescription.js
+++ src/components/Pillars/PillarDescription.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './PillarDescription.css';
 
-const PillarDescription = ({ backgroundColor, content }) => {  
+const PillarDescription = ({ backgroundColor, content, style}) => {  
   return (
     <section className="pillar-description-section" style={{ backgroundColor }}>
-      <p className="pillar-description-content">{content}</p>
+       <p className="pillar-description-content" style={style}>
+        {content}
+      </p>
     </section>
   );
 }
--- src/pages/Pillars/Culture/Culture.js
+++ src/pages/Pillars/Culture/Culture.js
@@ -11,6 +11,7 @@ import TabPanel from '../../../components/TabPanel/TabPanel';
 import './Culture.css';
 
 import cultureLogo from '../../../assets/logos/culture.png';
+import FacebookBlogImage from './UFFSA CULTURE.jpg';
 // import FAHM from './FAHM.jpeg';
 // import barrio from './barrio.jpeg';
 
@@ -36,6 +37,7 @@ const Culture = () => {
 
         <PillarDescription 
             backgroundColor={'var(--white)'}
+            style={{ textAlign: 'justify' }}
             content="Welcome to the UFFSA culture page! We will be posting information about Filipino culture, Filipino history, and current events from the Philippines! We will also posting information about our huge fall culture event, Filipino American History Month. Also, get ready for the other big spring culture event, Barrio Fiesta! Get ready for a year of Filipino culture!"
         />
 
@@ -53,26 +55,34 @@ const Culture = () => {
 
             <TabPanel value={signatureEventsValue} index={0}>
                 <h2>Filipino American History Month</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                    sit amet blandit leo lobortis eget.
+                <p style={{ textAlign: 'justify' }}> Filipino-American History Month (FAHM) is a month-long celebration of 
+                    Filipino culture from September to October. The opening ceremony kicks 
+                    off the month with a day filled with coming together as a community, 
+                    celebrating our collective heritage! During the ceremony, we will be 
+                    having modern and cultural dances from the UF Filipino Student Association, 
+                    live singing performances, delicious food, and a presentation about Filipino culture. 
+                    We will also be having our Ate-Kuya-Ading reveal towards the end, 
+                    where we match first years with mentors within FSA to help guide them throughout 
+                    the year and onwards.
+
                 </p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                    sit amet blandit leo lobortis eget.
+                <p style={{ textAlign: 'justify' }}> Throughout the month, we host a variety of theme-related small events that 
+                    promote community for those interested in immersing themselves in Filipino 
+                    history and forming new bonds with fellow members. Nearing the end of the month, 
+                    we host the closing ceremony with an evening brimming with unforgettable performances 
+                    and opportunities for attendees to further embrace one another, expanding the Pamilya. 
+                    Following our closing event, the annual Def Talent Jam competition takes place alongside 
+                    other surprises!
+
                 </p>
             </TabPanel>
             <TabPanel value={signatureEventsValue} index={1}>
                 <h2>Barrio Fiesta</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit.
-                </p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Suspendisse malesuada lacus ex,
-                    sit amet blandit leo lobortis eget.
+                <p style={{ textAlign: 'justify' }}> Barrio Fiesta is a day-long event during the spring semester in celebration of 
+                    members of the Filipino community! There, participants can engage in a 
+                    cultural festival with stage performances, games with audience participation, 
+                    and intermission for food and merch stalls. Unique to this gathering, 
+                    there is a fashion show, during which models display a multitude of traditional clothing.
                 </p>
             </TabPanel>
 
@@ -93,26 +103,28 @@ const Culture = () => {
 
             <TabPanel value={regularActivitiesValue} index={0}>
                 <h2>Facebook Blogs</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                    sit amet blandit leo lobortis eget.
+                <p style={{ textAlign: 'justify' }}> Join our Facebook group  <a href="https://www.facebook.com/groups/uffsaculture/" target="_blank">"UFFSA CULTURE"</a> to be updated on major events we are hosting or 
+                    celebrating this year! We are excited to showcase 
+                    what we have planned to both new members of the group 
+                    and our beloved alumni. Hope to see you become a member! 
                 </p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                    sit amet blandit leo lobortis eget.
-                </p>
+                <img 
+                    src={FacebookBlogImage}
+                    alt="UFFSA CULTURE" 
+                    style={{ width: '100%', marginTop: '20px' }} 
+                />
             </TabPanel>
             <TabPanel value={regularActivitiesValue} index={1}>
                 <h2>MAFFIA</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                    sit amet, consectetur adipiscing elit.
-                </p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget.  Suspendisse malesuada lacus ex,
-                    sit amet blandit leo lobortis eget.
+                <p style={{ textAlign: 'justify' }}> Want to work behind-the-scenes? Join MAFFIA! 
+                    Our program is suited for first-time Pamilya members 
+                    to get more involved with planning our wonderful FAHM activities 
+                    and memorable culture segments for the GBMs!
+                    Work alongside other MAFFIA members and We-Board to 
+                    not only produce a great event, but to also gain bonds 
+                    that could last a lifetime. 
+                    Be sure to be on the look-out for MAFFIA applications 
+                    that will be posted on our Instagram <a href="https://www.instagram.com/uffsa" target="_blank">(UFFSA)</a>.
                 </p>
             </TabPanel>
 
